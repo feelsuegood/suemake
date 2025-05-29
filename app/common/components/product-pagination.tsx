a411146998d1a10514dbccea66a9557ea0e001e6
feelsuegood <129813530+feelsuegood@users.noreply.github.com>
2025-05-29 17:50:56 +1000

    update pagination - useSearchParams

--- app/common/components/product-pagination.tsx
+++ app/common/components/product-pagination.tsx
@@ -1,3 +1,14 @@
+import { useSearchParams } from "react-router";
+import {
+  Pagination,
+  PaginationEllipsis,
+  PaginationItem,
+  PaginationLink,
+  PaginationNext,
+  PaginationPrevious,
+  PaginationContent,
+} from "./ui/pagination";
+
 type ProductPaginationProps = {
   totalPage: number;
 };
@@ -5,9 +16,92 @@ type ProductPaginationProps = {
 export default function ProductPagination({
   totalPage,
 }: ProductPaginationProps) {
+  const [searchParams, setSearchParams] = useSearchParams();
+  //   console.log(searchParams);
+  const page = Number(searchParams.get("page")) ?? 1;
+  if (isNaN(page) || page < 1) {
+    return null;
+  }
+  const onClick = (page: number) => {
+    searchParams.set("page", page.toString());
+    //* prevent scroll up to top
+    // setSearchParams(searchParams, { preventScrollReset: true });
+    //* ?page=3&hello=world -> hello=world is not lost by using setSearchParams
+    setSearchParams(searchParams);
+  };
   return (
     <div>
-      <div>ProductPagination</div>
+      <Pagination>
+        <PaginationContent>
+          {page === 1 ? null : (
+            <>
+              <PaginationItem>
+                <PaginationPrevious
+                  to={`?page=${page - 1}`}
+                  onClick={(event) => {
+                    event.preventDefault();
+                    onClick(page - 1);
+                  }}
+                />
+              </PaginationItem>
+              <PaginationItem>
+                <PaginationLink
+                  to={`?page=${page - 1}`}
+                  onClick={(event) => {
+                    event.preventDefault();
+                    onClick(page - 1);
+                  }}
+                >
+                  {page - 1}
+                </PaginationLink>
+              </PaginationItem>
+            </>
+          )}
+          <PaginationItem>
+            <PaginationLink
+              to={`?page=${page}`}
+              isActive
+              onClick={(event) => {
+                event.preventDefault();
+                onClick(page);
+              }}
+            >
+              {page}
+            </PaginationLink>
+          </PaginationItem>
+          {page === totalPage ? null : (
+            <>
+              <PaginationItem>
+                <PaginationLink
+                  to={`?page=${page + 1}`}
+                  onClick={(event) => {
+                    event.preventDefault();
+                    onClick(page + 1);
+                  }}
+                >
+                  {page + 1}
+                </PaginationLink>
+              </PaginationItem>
+              {page === totalPage - 1 ? null : (
+                <>
+                  <PaginationItem>
+                    <PaginationEllipsis />
+                  </PaginationItem>
+                </>
+              )}
+              <PaginationItem>
+                <PaginationNext
+                  to={`?page=${page + 1}`}
+                  onClick={(event) => {
+                    event.preventDefault();
+                    onClick(page + 1);
+                  }}
+                />
+              </PaginationItem>
+            </>
+          )}
+        </PaginationContent>
+      </Pagination>
     </div>
   );
 }
