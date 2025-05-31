88237fb592fec4d77913e4148c1a10cbc914f705
feelsuegood <129813530+feelsuegood@users.noreply.github.com>
2025-05-31 13:36:06 +1000

    update submit page

--- app/features/products/pages/submit-page.tsx
+++ app/features/products/pages/submit-page.tsx
@@ -11,6 +11,9 @@ import {
 } from "~/common/components/ui/select";
 import SelectPair from "~/common/components/select-pair";
 import { useState } from "react";
+import { Input } from "~/common/components/ui/input";
+import { Label } from "~/common/components/ui/label";
+import { Button } from "~/common/components/ui/button";
 
 export const meta: Route.MetaFunction = () => {
   return [
@@ -19,7 +22,15 @@ export const meta: Route.MetaFunction = () => {
   ];
 };
 
-export default function SubmitPage({ actionData }: Route.ComponentProps) {
+export default function SubmitPage() {
+  const [icon, setIcon] = useState<string | null>(null);
+  const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    if (e.target.files) {
+      const file = e.target.files[0];
+      //* get url of the file in the memory region of the browser
+      setIcon(URL.createObjectURL(file));
+    }
+  };
   return (
     <div>
       <Hero
@@ -77,6 +88,38 @@ export default function SubmitPage({ actionData }: Route.ComponentProps) {
               { label: "Design", value: "design" },
             ]}
           />
+          <Button type="submit" className="w-full size-lg ">
+            Submit
+          </Button>
+        </div>
+        <div className="flex flex-col space-y-5">
+          <div className="size-40 rounded-xl shadow-xl overflow-hidden">
+            {icon ? (
+              <img src={icon} className="w-full h-full object-cover" />
+            ) : null}
+          </div>
+          <Label className="flex flex-col gap-2">
+            Icon
+            <small className="text-muted-foreground">
+              This is the icon of your product
+            </small>
+          </Label>
+          <Input
+            type="file"
+            className="w-1/2"
+            onChange={onChange}
+            required
+            name="icon"
+          />
+          <div className="flex flex-col gap-1 text-xs">
+            <span className="text-muted-foreground">
+              Recommended size: 128x128px
+            </span>
+            <span className="text-muted-foreground">
+              Allowed formats: PNG, JPEG
+            </span>
+            <span className="text-muted-foreground">Max file size: 1MB</span>
+          </div>
         </div>
       </Form>
     </div>
