570cd381365b4152e052dfb6cfa28f6da032bd96
feelsuegood <129813530+feelsuegood@users.noreply.github.com>
2025-06-01 21:05:10 +1000

    create write-review-dialog

---
+++ app/features/products/components/create-review-dialog.tsx
@@ -0,0 +1,78 @@
+import { Form } from "react-router";
+import InputPair from "~/common/components/input-pair";
+import { Button } from "~/common/components/ui/button";
+import {
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+} from "~/common/components/ui/dialog";
+import { Label } from "~/common/components/ui/label";
+import { StarIcon } from "lucide-react";
+import { useState } from "react";
+
+export default function CreateReviewDialog() {
+  const [rating, setRating] = useState(0);
+  const [hoveredStar, setHoveredStar] = useState(0);
+  return (
+    <DialogContent>
+      <DialogHeader>
+        <DialogTitle className="text-2xl">
+          What do you think about this product?
+        </DialogTitle>
+        <DialogDescription>
+          Please share your thoughts with us.
+        </DialogDescription>
+      </DialogHeader>
+      <Form className="space-y-10">
+        <div>
+          <Label className="flex flex-col gap-1">
+            Rating
+            <small className="text-muted-foreground">
+              What would you rate this product?
+            </small>
+          </Label>
+          <div className="flex gap-2 mt-5">
+            {[1, 2, 3, 4, 5].map((star) => (
+              <label
+                key={star}
+                className="relative"
+                onMouseEnter={() => setHoveredStar(star)}
+                onMouseLeave={() => setHoveredStar(0)}
+              >
+                <StarIcon
+                  className="size-5 text-yellow-400"
+                  fill={
+                    hoveredStar >= star || rating >= star
+                      ? "currentColor"
+                      : "none"
+                  }
+                />
+                <input
+                  type="radio"
+                  value={star}
+                  name="rating"
+                  required
+                  className="opacity-0 h-px w-px absolute"
+                  onChange={() => setRating(star)}
+                />
+              </label>
+            ))}
+          </div>
+        </div>
+        <InputPair
+          textArea
+          required
+          label="Review"
+          name="review"
+          description="Maximum 500 characters"
+          placeholder="Write your review here..."
+        />
+        <DialogFooter>
+          <Button type="submit">Submit review</Button>
+        </DialogFooter>
+      </Form>
+    </DialogContent>
+  );
+}
--- app/features/products/pages/product-reviews-page.tsx
+++ app/features/products/pages/product-reviews-page.tsx
@@ -1,5 +1,14 @@
 import { Button } from "~/common/components/ui/button";
 import { ReviewCard } from "../components/review-card";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "~/common/components/ui/dialog";
+import CreateReviewDialog from "../components/create-review-dialog";
 
 export const meta = () => {
   return [
@@ -10,25 +19,30 @@ export const meta = () => {
 
 export default function ProductReviewsPage() {
   return (
-    <div className="space-y-10 max-w-xl">
-      {/* header */}
-      <div className="flex justify-between items-center">
-        <h2 className="text-2xl font-bold">10 Reviews</h2>
-        <Button variant={"secondary"}>Write a review</Button>
+    <Dialog>
+      <div className="space-y-10 max-w-xl">
+        {/* header */}
+        <div className="flex justify-between items-center">
+          <h2 className="text-2xl font-bold">10 Reviews</h2>
+          <DialogTrigger>
+            <Button variant={"secondary"}>Write a review</Button>
+          </DialogTrigger>
+        </div>
+        <div className="space-y-20">
+          {Array.from({ length: 10 }).map((_, index) => (
+            <ReviewCard
+              avatarUrl="https://github.com/feelsuegood.png"
+              avatarFallback="S"
+              username="John Doe"
+              handle="username"
+              rating={5}
+              content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos."
+              createdAt="10 days ago"
+            />
+          ))}
+        </div>
       </div>
-      <div className="space-y-20">
-        {Array.from({ length: 10 }).map((_, index) => (
-          <ReviewCard
-            avatarUrl="https://github.com/feelsuegood.png"
-            avatarFallback="S"
-            username="John Doe"
-            handle="username"
-            rating={2}
-            content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos."
-            createdAt="10 days ago"
-          />
-        ))}
-      </div>
-    </div>
+      <CreateReviewDialog />
+    </Dialog>
   );
 }
