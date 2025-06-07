db2d2768bfc5cb8b2e489a4ec5effa36926fab36
feelsuegood <129813530+feelsuegood@users.noreply.github.com>
2025-06-07 16:57:43 +1000

    complete ideas page

--- app/features/users/pages/dashboard-ideas-page.tsx
+++ app/features/users/pages/dashboard-ideas-page.tsx
@@ -1,24 +1,26 @@
-import type { Route } from "../+types";
+import { IdeaCard } from "~/features/ideas/components/idea-card";
+import { Route } from "./+types/dashboard-ideas-page";
 
-export function loader({ request }: Route.LoaderArgs) {
-  return {};
-}
+export const meta: Route.MetaFunction = () => {
+  return [{ title: "Dashboard Ideas | suemake" }];
+};
 
-export function action({ request }: Route.ActionArgs) {
-  return {};
-}
-
-export function meta({}: Route.MetaFunction) {
-  return [];
-}
-
-export default function DashboardIdeasPage({
-  loaderData,
-  actionData,
-}: Route.ComponentProps) {
+export default function DashboardIdeasPage() {
   return (
-    <div>
-      <h1>Dashboard Ideas</h1>
+    <div className="space-y-5 h-full">
+      <h1 className="text-2xl font-semibold mb-6">Claimed Ideas</h1>
+      <div className="grid grid-cols-4 gap-6">
+        {Array.from({ length: 5 }).map((_, index) => (
+          <IdeaCard
+            key={`ideaId-${index}`}
+            id={`ideaId-${index}`}
+            title="A startup that creates a AI-powered generated mental health app, delivering personalized therapy sessions to users and practical tools to practice mindfulness and log their progress with graphical reports and peaceful vibe that gives users a sense of calmness and relaxation which can be helpful for users to reduce stress and anxiety."
+            viewCount={99}
+            postedTime="12 hours ago"
+            likeCount={16}
+          />
+        ))}
+      </div>
     </div>
   );
 }
