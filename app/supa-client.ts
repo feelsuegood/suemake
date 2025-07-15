850ac657e2a0a62563cf4b5e83a9a5d2df8ce423
feelsuegood <129813530+feelsuegood@users.noreply.github.com>
2025-07-15 20:44:29 +1000

    type fest: type override

--- app/supa-client.ts
+++ app/supa-client.ts
@@ -1,9 +1,29 @@
 import { createClient } from "@supabase/supabase-js";
-import { Database } from "database.types";
+import { MergeDeep, SetNonNullable, SetFieldType } from "type-fest";
+import { Database as SupabaseDatabase } from "database.types";
+
+type Database = MergeDeep<
+  SupabaseDatabase,
+  {
+    public: {
+      Views: {
+        community_post_list_view: {
+          Row: SetFieldType<
+            SetNonNullable<
+              SupabaseDatabase["public"]["Views"]["community_post_list_view"]["Row"]
+            >,
+            "author_avatar",
+            string | null
+          >;
+        };
+      };
+    };
+  }
+>;
 
 const client = createClient<Database>(
-    process.env.SUPABASE_URL!,
-    process.env.SUPABASE_ANON_KEY!
+  process.env.SUPABASE_URL!,
+  process.env.SUPABASE_ANON_KEY!
 );
 
 export default client;
